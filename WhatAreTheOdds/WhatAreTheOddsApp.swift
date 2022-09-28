1e25dc6347fb6455df87d97b9c6750a502110312
Thomas Lawless <55810024+thomaslawless@users.noreply.github.com>
2022-09-28 11:37:26 -0400

    Initial Commit

---
+++ WhatAreTheOdds/WhatAreTheOddsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatAreTheOddsApp.swift
+//  WhatAreTheOdds
+//
+//  Created by Thomas Lawless III on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct WhatAreTheOddsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
